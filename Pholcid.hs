2a63da816d4c84cfdee1820dd319056187981a02
OC4 <oc4@protonmail.com>
2018-12-30 23:28:19 -0500

    Pointer: experimented with file/contents data structure using Paths

--- Pholcid.hs
+++ Pholcid.hs
@@ -1,95 +1,109 @@
+{-# LANGUAGE DeriveFunctor #-}
+
+
 import System.Directory (getCurrentDirectory)
-import Path (parseAbsDir)
+import Control.Comonad
+import qualified System.FilePath as FP
+import Path
+import qualified Path.Internal
+import Data.Word
 
 main :: IO ()
 main = do
   putStrLn $ "Testing making route paths\n" ++ replicate 15 '-'
-  curDir <- getCurrentDirectory
-  curRoute <-
-  putStrLn curRoute
-
--- | Route
---
--- A pathway down to the currently focused directory
---
--- Base: if browsing a file system, then equal to $home
---       if browsing within dir, then equal to that dir
---
--- Note: the filepath parsers all fail on empty strings
---
---     Usually home directory but can be relative to the desired "base dir"
---     |
--- [Root]
---       \
---        [Node]     A intermediate directory
---              \    |
---               [Node]
---                     \
---                      [Node]                | Either:
---                            \               | Head - pointed at local data
---                             [Node]         | Node - pointing at dir
---                                   \        /
---                                    [Head] | [Node]
---
--- Note: based on the actual data constructors we can shove a head in the middle
--- of a chain. Which is a bit like storing some data "a" in the middle other than
--- a path segment. Its generally not a problem since the constructors are not
--- exposed outside of this module. There is also a function normalize below that
--- excises any middle heads in a path way incase that situation comes up.
---
--- Note: there is no functionality to having data on a node "up" the chain
--- towards the base.
---
--- Advantages of "Route" representation:
---   -
---   -
-data Route b t a
-  = Base (Path   b Dir)
-  | Node (Path Rel   t)   (Route b t a)
-  | Head (Path Rel   t) a (Route b t a)
-    deriving (Eq, Functor, Typeable)
+  pwd <- getCurrentDirectory
+  putStrLn $ "Current directory is:\n\t" ++ pwd
+  p <- parseAbsDir pwd
+  putStrLn $ show $ pathDepth p
+  let s = Pointer p ()
+  putStrLn $ show s
+  putStrLn $ show $ depth s
+  skipLn 3
+  putStrLn $ "Testing out swaping contents of pointer"
+  let z = s $> "Testing"
+  putStrLn $ show z
+  skipLn 3
+  putStrLn $ "Depth of root = " ++ (show $ depth root)
+ where
+  skipLn = putStr . concat . flip replicate "\n"
+ -- curDir
+ -- putStrLn curRoute
 
 
--- | Basic pretty-printing of route
-instance Show a => Show (Route b t a) where
-  show (Base p)     = "\tRoute: " ++ show p
-  show (Node p r)   = show r ++ show p
-  show (Head p c r) = show r ++ show p ++ "\n\tContents: " ++ show c
+-- | Pointers are simply Paths with possible extra data. All typeclass instances
+-- are defined under the assumption the extra data is file (meta)data or
+-- directory contents
+data Pointer b t val = Pointer (Path b t) val
+                       deriving (Eq, Ord, Functor)
 
 
--- |
-instance Functor (Route b t) where
-  fmap _ (Base p)     = Base p
-  fmap _ (Node p r)   = Node p r
-  fmap f (Head p c r) = Head p (f c) r
+toPath :: Pointer b t val -> Path b t
+toPath (Pointer p _) = p
 
 
-depth :: Route b t a -> Int
-depth (Base _)     = 0
-depth (Node _ r)   = depth r + 1
-depth (Head _ r a) = depth r + 1
+pathEq :: Pointer b t val -> Pointer b t bal -> Bool
+pathEq (Pointer p0 _) (Pointer p1 _) = p0 == p1
 
 
-up :: Int -> Route b t a -> Route b t a
-up _ (Base p)     = b
-up d (Node p r)   = case d of
-                      0 -> Node p r
-                      _ -> up (n-1) r
-up d (Head p a r) = case d of
-                      0 -> Head p a r
-                      _ -> up (n-1) r
+rootPath :: Path b t  -- DANGEROUS!
+rootPath = Path.Internal.Path "/"
 
 
-parentDir :: Route b t a -> Route b t a
-parentDir = up 1
+root :: Pointer b t ()
+root = return ()
 
 
-pathSeperator :: Char
-pathSeperator = '/'
+instance Applicative (Pointer b t) where
+  pure v = Pointer rootPath v
+  (Pointer _ f) <*> p = fmap f p
 
-absdirToRoute :: MonadThrow m => FilePath -> m (Route Abs Dir a)
-absdirToRoute fp = do
-  p <- parseAbsDir
 
-  -- soemthing with cases
-  -- take parent and dirname, check , and recursively build up the route
+instance Monad (Pointer b t) where
+  (Pointer p v) >>= f = f v
+
+
+instance Comonad (Pointer b t) where
+  extract (Pointer _ v) = v
+  extend f x@(Pointer p _) = Pointer p $ f x
+
+
+instance Show val => Show (Pointer b t val) where
+  show x = unlines $ [ "Pointer:"
+                     , "\tPath: " ++ (show $ toPath x)
+                     , "\tContents: " ++ (show $ extract x)
+                     ]
+
+
+pathDepth :: Path b t -> Word16
+pathDepth p = auxPathDepth 0 p
+           where
+              auxPathDepth :: Word16 -> Path b1 t1 -> Word16
+              auxPathDepth y p'
+                 | toFilePath p' == toFilePath rootPath
+                         = y
+                 | y > 10 = y
+                 | otherwise
+                         = auxPathDepth (y+1) (parent p')
+
+
+depth :: Pointer b t a -> Word16
+depth = pathDepth . toPath
+
+m
+swapPath :: Path b t -> Pointer b0 t0 val -> Pointer b t val
+swapPath p (Pointer _ val) = Pointer p val
+
+
+pathReturn :: Path b t -> Pointer b t ()
+pathReturn p = swapPath p $ return ()
+
+
+point :: (Path b t -> val) -> Path b t -> Pointer b t val
+point f p = Pointer p $ f p
+
+
+pointM :: Monad m => (Path b t -> m val) -> Path b t -> m (Pointer b t val)
+pointM f p = do
+  v <- f p
+  return $ Pointer p v
+
