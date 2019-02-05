e56676d37bf0a80c92e3d79708acc3447f494f35
OC4 <oc4@protonmail.com>
2019-02-05 17:05:30 -0500

    See prev commit message. (Also run: ./fm --recompile)

--- Fm.hs
+++ Fm.hs
@@ -4,11 +4,12 @@ module Fm where
 
 -- | Testing a recompile of a program from a config of a Haskell file.
 -- Based off xmonad
+import Control.Exception
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.Map (Map, fromList)
-import System.Directory (getCurrentDirectory)
+import System.Directory (getCurrentDirectory, getModificationTime)
 import System.Environment (getArgs)
-
+import System.FilePath ((</>))
 
 
 -- Currently, given any configuration settings, do nothing.
@@ -28,7 +29,7 @@ data Config = MkConf {
   }
 
 defaultConfig :: Config
-defaultConfig = Config {
+defaultConfig = MkConf {
     myInt = 0
   , howToSayHi = putStrLn
   , listOfEnemies = fromList $ [ (001, "Flea"), (002,"Ganon") ]
@@ -41,10 +42,18 @@ recompile s = do
   dir <- getFmDir
   return s
   let src = dir </> "fm_config.hs"
-      bin = dir </> "fm" 
+      bin = dir </> "fm"
+  srcMT <- getModTime src
+  binMT <- getModTime bin
+  io $ putStrLn $ show srcMT
+  io $ putStrLn $ show binMT
+  return True
   where
-    getFmDir = getCurrentDirectory
+    getFmDir = io getCurrentDirectory
 
+    -- For any file system error just return nothing
+    getModTime f = io $ catch (Just <$> getModificationTime f)
+                              (\(SomeException _) -> return Nothing)
 
 
 
@@ -60,5 +69,5 @@ unitA = pure ()
 
 unitM :: Monad m => m ()
 unitM = return ()
--}
+
 
