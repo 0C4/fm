15eda11c69a295b29f7e65f11267cd5ffc0eb0ca
OC4 <oc4@protonmail.com>
2019-02-06 11:46:02 -0500

    fm can recompile itself from edited config source file (compiles & executes)

--- Fm.hs
+++ Fm.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE
+    MultiWayIf,
+    LambdaCase
+#-}
 
 module Fm where
 
@@ -7,32 +11,42 @@ module Fm where
 import Control.Exception
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.Map (Map, fromList)
-import System.Directory (getCurrentDirectory, getModificationTime)
+import System.Directory (getCurrentDirectory, getModificationTime, renameFile,
+                         removeFile)
 import System.Environment (getArgs)
-import System.FilePath ((</>))
-import System.Process (waitForProcess, proc,)
+import System.Exit
+import System.FilePath ((</>),(<.>))
+import System.Process
+import GHC.IO.Handle
 
 -- Currently, given any configuration settings, do nothing.
 fm :: Config -> IO ()
 fm c = do
   args <- getArgs
-  if | args `elem` ["-r", "--recompile"] -> recompile True >>= putStrLn . show
-     | otherwise                         -> putStrLn $ show c
- where
+  case args of
+    ["--recompile"] -> recompile True >>= putStrLn . show
+    _               -> putStrLn $ show c
+
+
 
 
 -- Test config record.
 data Config = MkConf {
     myInt :: Int
-  , howToSayHi :: String -> IO ()
+  , howToSayHi :: String -> String
   , listOfEnemies :: Map Int String
   }
+
+instance Show Config where
+  show c = show (myInt c) ++ (unlines $ foldl (\x y-> x ++ [h y]) [] (listOfEnemies c))
+        where
+          h x = (howToSayHi c) x
 
 
 defaultConfig :: Config
 defaultConfig = MkConf {
     myInt = 0
-  , howToSayHi = putStrLn
+  , howToSayHi = id
   , listOfEnemies = fromList $ [ (001, "Flea"), (002,"Ganon") ]
   }
 
@@ -75,7 +89,7 @@ ghc = createProcess . proc "ghc"
 --        the souce directory
 --
 --
--- 'False' is returned if there are compilation errors. 
+-- 'False' is returned if there are compilation errors.
 --
 recompile :: MonadIO m => Bool -> m Bool
 recompile s = io $ do
@@ -83,12 +97,20 @@ recompile s = io $ do
   let src = dir </> "fm_config.hs"
       bin = dir </> "fm"
       make = dir </> "MAKE"
-  b <- waitForProcess undefined >>=
-           \case
-             ExitSuccess   -> return True
-             ExitFailure _ -> return False
-  \case
-    True  -> do {- atomic replacement -}
-    False -> do {- nothing/clean up   -}
+      temp = bin <.> "recompiling"
+      --cleanUp = mapM_ (catchIOErr removeFile) [src <.> "hi", src <.> "o"]
+  e <- waitForProcess =<< do (_,_,_,h)<- ghc ["-o",temp,src]
+                             return h
+  --
+  case e of
+     ExitSuccess   -> do
+       renameFile temp bin
+       cleanUp
+       return True
+     ExitFailure n -> do
+       cleanUp
+       return False
+
+ where
   --srcMT <- getModTime src
   --binMT <- getModTime bin
--- fm_config.hs
+++ fm_config.hs
@@ -1,17 +1,17 @@
 import Fm
 
-
+import Data.Map (fromList)
 
 -- Using Haskell source file as your config file:
 -- Basically writing your own main function.
 main :: IO ()
-main = fm defaultConfig
+main = fm {- defaultConfig -} myConfig
 
 
 
 myConfig :: Config
 myConfig = MkConf {
     myInt = 666
-  , howToSayHi = (\x -> putStrLn $ unwords ["HELLO",x,", YOU ROCK"]
-  , listOfEnemies = fromList $ [ (666, "SATAN"), (-001, "God") ]
+  , howToSayHi = \x -> unwords ["HELLO",x,", YOU ROCK"]
+  , listOfEnemies = fromList [ (666, "SATAN"), (-001, "God") ]
   }
